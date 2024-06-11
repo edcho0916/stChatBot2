3dfdf460ec908896c91eb9ae198b29a8b8b20867
edcho0916 <edcho0916@gmail.com>
2024-06-11 18:52:49 +0900

    Update and rename page1.py to StudentHistory.py

---
+++ pages/StudentHistory.py
@@ -0,0 +1,39 @@
+from openai import OpenAI
+import streamlit as st
+
+
+    
+# st.title("💬 학교생활기록부챗봇")
+# st.caption("🚀 A Streamlit chatbot powered by OpenAI")
+
+OpenAI_API_Key ='sk-proj-5ID6L8XujXgQxkbqMtBiT3BlbkFJW2CIB6F78ZrRFQJ3Bjju'
+
+client = OpenAI(api_key=OpenAI_API_Key)
+
+def assistantsID(chatbotName):
+    my_assistants = client.beta.assistants.list(
+    order="desc",
+    limit="20",
+    )
+    count = my_assistants.data.count('Assistant')
+    x=0
+    isFind = False
+    while x <= count:
+        if my_assistants.data[x].name == chatbotName:
+            return my_assistants.data[x].id
+
+assistant_ID = assistantsID('학교생활기록부챗봇')
+st.wirte(assistant_ID)
+
+# if prompt := st.chat_input():
+#     if not openai_api_key:
+#         st.info("Please add your OpenAI API key to continue.")
+#         st.stop()
+
+#     client = OpenAI(api_key=openai_api_key)
+#     st.session_state.messages.append({"role": "user", "content": prompt})
+#     st.chat_message("user").write(prompt)
+#     response = client.chat.completions.create(model="gpt-3.5-turbo", messages=st.session_state.messages)
+#     msg = response.choices[0].message.content
+#     st.session_state.messages.append({"role": "assistant", "content": msg})
+#     st.chat_message("assistant").write(msg)
