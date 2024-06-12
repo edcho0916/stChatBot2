7aa2c70263c2cbd0d18599815f1e0f654d2779d0
edcho0916 <edcho0916@gmail.com>
2024-06-12 10:47:13 +0900

    Update Homepage.py

--- Homepage.py
+++ Homepage.py
@@ -1,14 +1,18 @@
 import streamlit as st
 
+if "api_key" not in st.session_state:
+     st.session_state.api_key = None
+
 with st.sidebar:
-    openai_api_key = st.text_input("OpenAI API Key", key="chatbot_api_key", type="password")
+    openai_api_key = st.text_input("OpenAI API Key", key="chatbot_api_key", type="password", value=st.session_state.api_key)
+    if not openai_api_key:
+        st.info('Please input your OpenAI API key')
+        st.stop()
+    else:
+        st.info('Select ChatBot and Input prompt')
+        st.session_state.api_key = openai_api_key
     
 st.title("💬 Chatbot For Teacher")
 st.caption("🚀 A Streamlit chatbot powered by OpenAI")
 st.write('왼쪽 사이드바에서 해당 영역을 선택하고 OpenAI사의 APIKEY를 입력해 주세요. ')
 
-if "messages" not in st.session_state:
-    st.session_state["messages"] = [{"role": "assistant", "content": "How can I help you?"}]
-
-for msg in st.session_state.messages:
-    st.chat_message(msg["role"]).write(msg["content"])
