098692d1d5534c179d9a6dd8c6031e60a22d457d
edcho0916 <edcho0916@gmail.com>
2024-06-14 14:29:50 +0900

    Update _05초과근무.py

--- pages/_05초과근무.py
+++ pages/_05초과근무.py
@@ -22,34 +22,34 @@ if "messages_05" not in st.session_state:
 for msg in st.session_state.messages_05:
     st.chat_message(msg["role"]).write(msg["content"])
 if prompt := st.chat_input():
-   if not st.session_state.api_key:
+    if not st.session_state.api_key:
        st.info("Please input your OpenAI API key to continue: at Homepage")
        st.stop()
-   st.session_state.messages_05.append({"role": "user", "content": prompt})
-   st.chat_message("user").write(prompt)
+    st.session_state.messages_05.append({"role": "user", "content": prompt})
+    st.chat_message("user").write(prompt)
 
-   response = client.beta.threads.messages.create(
-   thread_id = st.session_state.thread_ID_05,
-   role = "user",
-   content = prompt,
-   )
-
-run_05 = client.beta.threads.runs.create(
-    thread_id = st.session_state.thread_ID_05, 
-    assistant_id = assistant_ID
-   )
-
-while True:
-    run_05 = client.beta.threads.runs.retrieve(
-        thread_id=st.session_state.thread_ID_05,
-        run_id = run_05.id
+    response = client.beta.threads.messages.create(
+    thread_id = st.session_state.thread_ID_05,
+    role = "user",
+    content = prompt,
     )
-    if run_05.status=="completed":
-        break
-    else:
-        time.sleep(2)
-thread_messages_05 = client.beta.threads.messages.list(st.session_state.thread_ID_05)
-msg = thread_messages_05.data[0].content[0].text.value
-print(thread_messages_05)
-st.session_state.messages_05.append({"role": "assistant", "content": msg})
-st.chat_message("assistant").write(msg)
+
+    run_05 = client.beta.threads.runs.create(
+        thread_id = st.session_state.thread_ID_05, 
+        assistant_id = assistant_ID
+    )
+
+    while True:
+        run_05 = client.beta.threads.runs.retrieve(
+            thread_id=st.session_state.thread_ID_05,
+            run_id = run_05.id
+        )
+        if run_05.status=="completed":
+            break
+        else:
+            time.sleep(2)
+    thread_messages_05 = client.beta.threads.messages.list(st.session_state.thread_ID_05)
+    msg = thread_messages_05.data[0].content[0].text.value
+    print(thread_messages_05)
+    st.session_state.messages_05.append({"role": "assistant", "content": msg})
+    st.chat_message("assistant").write(msg)
