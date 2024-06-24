ef95c20aec460a5e0e7114df93babcb6631503cb
edcho0916 <edcho0916@gmail.com>
2024-06-24 12:00:19 +0900

    Update Homepage.py

--- Homepage.py
+++ Homepage.py
@@ -1,19 +1,22 @@
 import streamlit as st
 
-if "api_key" not in st.session_state:
-    st.session_state.api_key = None
+if "password" not in st.session_state:
+    st.session_state.password = None
 
 st.title("Welcome!!!")
 st.title("💬 Chatbot For Teacher")
 st.caption("🚀 A Streamlit chatbot powered by OpenAI")
-st.write('왼쪽 사이드 바에서 1. API key 입력, 2. 상담 영역 선택.')
+st.write('왼쪽 사이드 바에서 1. PASSWORD 입력, 2. 상담 영역 선택.')
 
 with st.sidebar:
-    openai_api_key = st.text_input("OpenAI API Key", key="chatbot_api_key", type="password", value=st.session_state.api_key)
-    if not openai_api_key:
-        st.info('Please input your OpenAI API key')
+    password = st.text_input("OpenAI API Key", key="chatbot_api_key", type="password", value=st.session_state.api_key)
+    if not password :
+        st.info('Please input your PASSWORD')
         st.stop()
-    else:
+    elif st.secrets['PASSWORD']==password:
         st.info('Select ChatBot and Input prompt')
-        st.session_state.api_key = openai_api_key
+        st.session_state.api_key = st.secrets{'API_KEY']
+    else:
+        st.info('Input correct password!!!')
+        st.stop()
     
